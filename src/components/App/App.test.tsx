1c9f503ab696a3652e8d4e28da569adf741049b2
bobrovice <kirill.madze@gmail.com>
2019-11-16 13:30:26 +0300

    App test fix

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Redirect, Route} from 'react-router';
+import Login from '../Login/Login';
+import Authorize from '../Authorize/Authorize';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((<Router>
+    <div>
+      <Route path="/login" component={Login}/>
+      <Route exact path="/">
+        <Redirect to="/login"/>
+      </Route>
+      <Route path="/clan/:clan" component={App}/>
+      <Route path="/authorization" component={Authorize}/>
+    </div>
+  </Router>), div);
   ReactDOM.unmountComponentAtNode(div);
 });
